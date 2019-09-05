936bf94f7d3dad826f487c69f7c7a7ec99f14bfa
bevinart <42657361+bevinart@users.noreply.github.com>
2019-09-05 16:46:11 -0400

    Added many functions to server.lua
    -Checks Bans inside of MySQL Database
    -Changed CancelEvent() to deferrals.done()
    -Checks if Player is added to the Database
    -If player was banned, prevent them from connecting and say you were banned followed by a reason, admin that did the ban, and time left
    -Added deferrals.update() along down the code (*its actually like two places but shut*)
    -Removed some pretty useless print functions btw :)

--- server.lua
+++ server.lua
@@ -1,30 +1,92 @@
---[[BUGS:
-
-
-
-    ADD:
-	Keep track of bans, and prevent people from joining if they were banned.
-	Make temp bans.
---]]
-
 local function OnPlayerConnecting(name, setReason, deferrals)
-	local identifiers, steamIdentifier = GetPlayerIdentifiers(source)
+	local banTime = ""
+	local banReason = ""
+	local banAdmin = ""
 
+	deferrals.defer()
+	deferrals.update("Checking Steam...")
+
+	local identifiers = GetPlayerIdentifiers(source)
+
+	local steamIdentifier = SteamCheck(identifiers, steamIdentifier)
+
+	if steamIdentifier == nil then
+		deferrals.done("You must have Steam open to be able to play on this FiveM server, thank you bossman B)")
+
+	else
+		deferrals.update("Checking Database...")
+		checkBan, banReason, banAdmin, banTime = BanCheck(steamIdentifier)
+
+		if checkBan == true then
+			deferrals.done("YOU ARE BANNED FROM PROJECT REALITY\n\n Reason: " .. banReason .. "\nBy: " .. banAdmin .. "\nTime: " .. banTime)
+
+		else
+			PlayerCheck(name, steamIdentifier)
+			deferrals.done()
+		end
+	end
+end
+
+
+function SteamCheck(identifiers, steamIdentifier)
 	for _,v in pairs(identifiers)do
-		print(v)
 		if string.match(v, "steam") then
 			steamIdentifier = v
 			break
 		end
 	end
-	print(steamIdentifier)
-
-	if steamIdentifier == nil then
-		setReason("You must have Steam open to be able to play on this FiveM server, thank you bossman B)")
-		CancelEvent()
+	return steamIdentifier
+end
 
 
+function BanCheck(steamIdentifier, banReason, banAdmin, banTime)
+	local count = 0
+
+	local findBan = MySQL.Sync.fetchAll("SELECT * FROM bans", {})
+	for _, __ in pairs(findBan) do
+		count = count + 1
+		print(findBan[count].SteamID)
+		if findBan[count].SteamID == steamIdentifier then
+			banReason = findBan[count].Reason
+			banAdmin = findBan[count].Admin
+			banTime = findBan[count].BanTime
+			return true, banReason, banAdmin, banTime
+		end
+	end
+
+	return false
+end
+
+
+function PlayerCheck(name, steamIdentifier)
+	local count = 0
+	local found = false
+	local searchPlayers = MySQL.Sync.fetchAll("SELECT * FROM players", {})
+
+	for _, __ in pairs(searchPlayers) do
+		count = count + 1
+
+		if searchPlayers[count].SteamID == steamIdentifier then
+			found = true
+			break
+		end
+	end
+
+	if found == false then
+		count = 0
+		while true do
+			count = count + 1
+			randomDCode = math.random(10000, 99999)
+			if searchPlayers == nil or searchPlayers[count].DiscordCode ~= randomDCode then
+				MySQL.Sync.fetchAll("INSERT INTO players (SteamName, SteamID, DiscordCode, Rank) VALUES (@steamname, @steamid, @discordcode, @rank)", {["@steamname"] = name, ["@steamid"] = steamIdentifier, ["@discordcode"] = randomDCode, ["@rank"] = "Citizen"})
+				break
+
+			elseif searchPlayers[count].DiscordCode == randomDCode then
+				repeat until false
+			end
+		end
 	end
 end
+
 
 AddEventHandler("playerConnecting", OnPlayerConnecting)
